f7653bafeacdc60afc40ffac77de98e1dcb01c44
Vitor Almeida <vitor.almeida@redeflex.com.br>
2025-08-05 10:56:20 -0400

    Add component navbar

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Button.css'
+
+interface TextButton {
+    text: string;
+}
+
+const Button: React.FC<TextButton> = ({text}) => {
+    return (
+        <button className="button">
+            <div className="blob1"></div>
+            <div className="blob2"></div>
+            <div className="inner">{text}</div>
+        </button>
+    )
+}
+
+export default Button
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-scroll'
+import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faCode } from '@fortawesome/free-solid-svg-icons'
+import Button from '../Button/Button'
+
+const Navbar = () => {
+
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setSticky(window.scrollY > 50);
+        })
+    }, []);
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
+    return (
+        <nav className={`site-container ${sticky ? 'dark-nav' : ''}`}>
+            <div className='logo'>
+                <FontAwesomeIcon className='logo-icon' icon={faCode} />
+                <div className='text-logo'>
+                    <h1>Portfólio Vitor Almeida</h1>
+                </div>
+            </div>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+                <li><Link to='home' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='about' smooth={true} offset={-360} duration={500}>Sobre</Link></li>
+                <li><Link to='skills' smooth={true} offset={-160} duration={500}>Skills</Link></li>
+                <li><Link to='portfolio' smooth={true} offset={-260} duration={500}>Portfólio</Link></li>
+                <li>
+                    <Link to='contact' smooth={true} offset={-260} duration={500}>
+                       <Button text='Contato'></Button>
+                    </Link>
+                </li>
+            </ul>
+            <FontAwesomeIcon icon={faBars} className='menu-icon' onClick={toggleMenu} />
+        </nav>
+    )
+}
+
+export default Navbar
