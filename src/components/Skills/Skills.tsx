33a67101483544db5562336001fe87c6e4f1a646
Vitor Almeida <vitor.almeida@redeflex.com.br>
2025-08-12 16:46:26 -0400

    ajustes

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -7,6 +7,7 @@ import csharp_img from '../../assets/img/csharp-logo.png'
 import dotnet_img from '../../assets/img/NET-Framework-Logo.png'
 import typescript_img from '../../assets/img/typescript.png'
 import react_img from '../../assets/img/react.png'
+import node_img from '../../assets/img/nodejs.png'
 import mySql_img from '../../assets/img/mysql.png'
 import postgre_img from '../../assets/img/postgreSql.png'
 import sqlServer_img from '../../assets/img/sqlserver.png'
@@ -18,20 +19,31 @@ const Skills = () => {
           
           <div className='skills-bars'>
             <h1>Técnica</h1>
-
-            <div className='skills-bars-item'>
-              <div className='skills-tech'>
-                <img className='img-logo dotnet' src={dotnet_img} width={30}/><ProgressBar skill=".NET" percentage={85} /> 
-                <img className='img-logo' src={typescript_img} width={30}/><ProgressBar skill="TypeScript" percentage={80} /> 
-                <img className='img-logo' src={react_img} width={30}/><ProgressBar skill="React" percentage={70} /> 
-                <img className='img-logo' src={react_img} width={30}/><ProgressBar skill="React" percentage={70} /> 
-              </div>
-              <div className='skills-data-base'>
-                <img className='img-logo' src={mySql_img} width={30}/><ProgressBar skill="MySQL" percentage={85} /> 
-                <img className='img-logo' src={postgre_img} width={30}/><ProgressBar skill="PostgreeSQL" percentage={80} /> 
-                <img className='img-logo' src={sqlServer_img} width={20}/><ProgressBar skill="SQLServer" percentage={70} /> 
-                <img className='img-logo' src={react_img} width={30}/><ProgressBar skill="React" percentage={70} /> 
-              </div>
+            <div className='skills-tech'>
+                <div className='skills-bars-item'>
+                  <img className='img-logo dotnet' src={dotnet_img} width={30}/><ProgressBar skill=".NET" percentage={85} /> 
+                </div>
+                <div className='skills-bars-item'>
+                  <img className='img-logo' src={csharp_img} width={30}/><ProgressBar skill="CSharp" percentage={85} /> 
+                </div>
+                <div className='skills-bars-item'>
+                  <img className='img-logo' src={typescript_img} width={30}/><ProgressBar skill="TypeScript" percentage={85} /> 
+                </div>
+                <div className='skills-bars-item'>
+                  <img className='img-logo' src={react_img} width={30}/><ProgressBar skill="React" percentage={85} /> 
+                </div>
+                <div className='skills-bars-item'>
+                  <img className='img-logo' src={node_img} width={45}/><ProgressBar skill="Node.js" percentage={85} /> 
+                </div>
+                <div className='skills-bars-item'>
+                  <img className='img-logo' src={mySql_img} width={30}/><ProgressBar skill="MySQL" percentage={85} /> 
+                </div>
+                <div className='skills-bars-item'>
+                  <img className='img-logo' src={postgre_img} width={30}/><ProgressBar skill="PostgreSQL" percentage={85} /> 
+                </div>
+                <div className='skills-bars-item'>
+                  <img className='img-logo' src={sqlServer_img} width={30}/><ProgressBar skill="SQLServer" percentage={85} /> 
+                </div>
             </div>
           </div>
 
@@ -40,19 +52,19 @@ const Skills = () => {
             <div className='skill-circe-container'>
               <div className='skill-circe-item'>
                 <CircularProgressBar skill="" percentage={65} />
-                <p>MySQL</p>
+                <p>Trabalho em Equipe</p>
               </div>
               <div className='skill-circe-item'>
                 <CircularProgressBar skill="" percentage={65} />
-                <p>MySQL</p>
+                <p>Foco em Resultados</p>
               </div>
               <div className='skill-circe-item'>
                 <CircularProgressBar skill="" percentage={65} />
-                <p>MySQL</p>
+                <p>Resolução de Problemas</p>
               </div>
                <div className='skill-circe-item'>
                 <CircularProgressBar skill="" percentage={65} />
-                <p>MySQL</p>
+                <p>Adaptabilidade</p>
               </div>
             </div>
           </div>
