c4b0c9f97cb68e511070cf8a2d2a48149833e919
Vitor Almeida <vitor.almeida@redeflex.com.br>
2025-08-13 16:48:03 -0400

    add component services

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home"
 import Title from "./components/Title/Title"
 import About from "./components/About/About"
 import Skills from "./components/Skills/Skills"
+import Services from "./components/Services/Services"
 
 function App() {
 
@@ -20,6 +21,12 @@ function App() {
       <div className="site-container">
         <Title subTitle="Skills" title="Minhas Skills" classColor=""></Title>
         <Skills/>
+      </div>
+      <div className="back-black">
+        <div className="site-container">
+          <Title subTitle="Serviços" title="Minhas Especialidades" classColor=""></Title>
+          <Services/>
+        </div>
       </div>
     </div>
   )
